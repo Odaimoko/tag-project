cb302e8733d577e2fb76d58fe69a9bc9a9ade4f2
oda <odaimoko@qq.com>
2023-10-14 18:24:35 +0800

    add: ONotice supports DocumentFragment

--- src/utils/o-notice.ts
+++ src/utils/o-notice.ts
@@ -2,7 +2,23 @@ import {Notice} from "obsidian";
 import {PLUGIN_NAME} from "../main";
 
 export class ONotice extends Notice {
-    constructor(message: string) {
-        super(`[${PLUGIN_NAME}] ${message}`);
+    /**
+     *
+     * @param message
+     * @param duration seconds
+     */
+    constructor(message: string | DocumentFragment, duration?: number) {
+        const durInSec = duration ? duration * 1000 : duration;
+        if (message instanceof String)
+            super(`[${PLUGIN_NAME}] ${message}`, durInSec);
+        else {
+            const doc = message as DocumentFragment;
+            doc.prepend(`[${PLUGIN_NAME}] `);
+            super(message, durInSec);
+        }
     }
+}
+
+export function notify(message: string | DocumentFragment, duration?: number) {
+    new ONotice(message, duration);
 }
