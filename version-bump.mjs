472f7247d2c493dbcf1252603d35429b1828c5ad
oda <odaimoko@qq.com>
2023-10-20 03:36:24 +0800

    add: version-bump.mjs also adds tag and pushes to origin

--- version-bump.mjs
+++ version-bump.mjs
@@ -1,11 +1,12 @@
 import {readFileSync, writeFileSync} from "fs";
+import {exec} from "child_process";
 
 // This is the version defined in package.json
 const targetVersion = process.env.npm_package_version;
 
 // read minAppVersion from manifest.json and bump version to target version
 let manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
-const { minAppVersion } = manifest;
+const {minAppVersion} = manifest;
 manifest.version = targetVersion;
 writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
 
@@ -13,3 +14,9 @@ writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
 let versions = JSON.parse(readFileSync("versions.json", "utf8"));
 versions[targetVersion] = minAppVersion;
 writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
+
+exec("git add manifest.json package.json versions.json")
+exec(`git commit -m "Bump version to ${targetVersion}"`)
+exec("git push")
+exec(`git tag -a v${targetVersion} -m "Bump version tag to ${targetVersion}"`)
+exec("git push --tags")
