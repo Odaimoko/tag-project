4cc65b6e3c205ef5898840af17e181efe2140fdf
oda <odaimoko@qq.com>
2023-11-19 21:14:46 +0800

    add: function getProjectByTaskPath and its unit test.

--- src/data-model/OdaProjectTree.ts
+++ src/data-model/OdaProjectTree.ts
@@ -1,18 +1,32 @@
 import {OdaPmTask} from "./OdaPmTask";
 import {OdaPmProject} from "./OdaPmProject";
 import {I_OdaPmWorkflow} from "./workflow-def";
+import * as path from "path";
 
+
+/**
+ *
+ * @param projectDict Path to OdaPmProject
+ * @param taskPath
+ */
+export function getProjectByTaskPath<T>(projectDict: Record<string, T>, taskPath: string) {
+
+    while (!projectDict.hasOwnProperty(taskPath)) {
+        taskPath = path.dirname(taskPath);
+    }
+    return projectDict[taskPath]
+}
 
 export class OdaProjectTree {
-    static buildProjectShadowTree(): OdaProjectTree {
-        return undefined;
+    static buildProjectShadowTree(): OdaProjectTree | null {
+        return null;
     }
 
-    getProjectByPmTask(pmTask: OdaPmTask): OdaPmProject {
-        return undefined;
+    getProjectByPmTask(pmTask: OdaPmTask): OdaPmProject | null {
+        return null;
     }
 
-    getProjectByPmWorkflow(pmWorkflow: I_OdaPmWorkflow): OdaPmProject {
-        return undefined;
+    getProjectByPmWorkflow(pmWorkflow: I_OdaPmWorkflow): OdaPmProject | null {
+        return null;
     }
 }
---
+++ src/data-model/__tests__/find-project-by-path.ts
@@ -0,0 +1,96 @@
+import {describe, expect, test} from "@jest/globals";
+import {getProjectByTaskPath} from "../OdaProjectTree";
+
+/**
+ * - Unclassified. path:`/`
+ *    - File `/file_any`
+ *        - Task (Prj4), `/file_any:Prj4`
+ *    - Folder (1), with def (Prj1). path: `/folder1`
+ *        - File, `tpm_projectroot: Prj1`. `./folder1/file1`
+ *        - File def (Prj2). `/folder1/file2`
+ *            - Task. (Prj2)
+ *        - File. `/folder1/file3`
+ *            - Task. (Prj1). `/folder1/file3`
+ *            - Task, def (Prj3). `/folder1/file3:Prj3`
+ *        - File def (Prj3). `/folder1/file4`
+ *            - Task (Prj3). `/folder1/file4`
+ */
+interface MockProject {
+    name: string;
+    paths: string[]
+}
+
+const projects: Record<string, MockProject> = {
+    Unclassified: {
+        name: "Unclassified",
+        paths: ["/"],
+    },
+    Prj1: {
+        name: "Prj1",
+        paths: ["/folder1"],
+    },
+    Prj2: {
+        name: "Prj2",
+        paths: ["/folder1/file2"],
+    },
+    Prj3: {
+        name: "Prj3",
+        paths: ["/folder1/file3:Prj3", "/folder1/file4"],
+    },
+    Prj4: {
+        name: "Prj4",
+        paths: ["/file_any:Prj4"],
+    }
+}
+
+// can be substituted 
+function getNameByProject(project: MockProject) {
+    return project['name']
+}
+
+
+function getProjectNameByProjectPath(projectPath: string) {
+    const projectDict: Record<string, MockProject> = {} // Path to project
+    for (const projectName of Object.keys(projects)) {
+        const project = projects[projectName];
+        for (const path of project.paths) {
+            projectDict[path] = project
+        }
+    }
+    const p = getProjectByTaskPath(projectDict, projectPath);
+    return getNameByProject(p)
+}
+
+describe("Match tags", () => {
+
+    test("Root -> Unclassified", () => {
+        expect(getProjectNameByProjectPath('/')).toBe('Unclassified')
+    });
+    test("Any file under / -> Unclassified", () => {
+        expect(getProjectNameByProjectPath('/file_any')).toBe('Unclassified')
+    });
+    test("Task def Prj4 under / -> Prj4", () => {
+        expect(getProjectNameByProjectPath('/file_any:Prj4')).toBe('Prj4')
+    });
+    test("Any file under Any folder -> Unclassified", () => {
+        expect(getProjectNameByProjectPath('/folder_any/file_any')).toBe('Unclassified')
+    });
+    test("Folder Prj1 -> Prj1", () => {
+        expect(getProjectNameByProjectPath('/folder1/file_any')).toBe('Prj1')
+    });
+    test("File1 under Prj1 -> Prj1", () => {
+        expect(getProjectNameByProjectPath('/folder1/file1')).toBe('Prj1')
+    });
+    test("Task in file3 under Prj1 -> Prj1", () => {
+        expect(getProjectNameByProjectPath('/folder1/file3')).toBe('Prj1')
+    });
+    test("File def Prj2 under Prj1 -> Prj2", () => {
+        expect(getProjectNameByProjectPath('/folder1/file2')).toBe('Prj2')
+    });
+    test("Task def Prj3 in File 3 -> Prj3", () => {
+        expect(getProjectNameByProjectPath('/folder1/file3:Prj3')).toBe('Prj3')
+    });
+    test("Any file under Prj3 -> Prj3", () => {
+        expect(getProjectNameByProjectPath('/folder1/file4')).toBe('Prj3')
+    })
+})
