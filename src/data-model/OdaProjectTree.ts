204d6381a0d724ed681784ae5921374d92e3678d
oda <odaimoko@qq.com>
2023-11-20 09:21:10 +0800

    fix: match the find-project-by-path tests with the feature spec.
    A task is always in a file so it cannot have the name `/`.

--- src/data-model/OdaProjectTree.ts
+++ src/data-model/OdaProjectTree.ts
@@ -5,12 +5,13 @@ import * as path from "path";
 
 
 /**
- *
+ * The root is /.
  * @param projectDict Path to OdaPmProject
  * @param taskPath
  */
 export function getProjectByTaskPath<T>(projectDict: Record<string, T>, taskPath: string) {
-
+    // This is checking implementation: if a task has no project defined with it or in its file, 
+    //      it will find projects in its parent directory.
     while (!projectDict.hasOwnProperty(taskPath)) {
         taskPath = path.dirname(taskPath);
     }
--- src/data-model/__tests__/find-project-by-path.ts
+++ src/data-model/__tests__/find-project-by-path.ts
@@ -4,6 +4,7 @@ import {getProjectByTaskPath} from "../OdaProjectTree";
 /**
  * - Unclassified. path:`/`
  *    - File `/file_any`
+ *        - Task (Unclassified). path: `/file_any`
  *        - Task (Prj4), `/file_any:Prj4`
  *    - Folder (1), with def (Prj1). path: `/folder1`
  *        - File, `tpm_projectroot: Prj1`. `./folder1/file1`
@@ -61,36 +62,40 @@ function getProjectNameByProjectPath(projectPath: string) {
     return getNameByProject(p)
 }
 
-describe("Match tags", () => {
+describe("Get Project By Task Path", () => {
 
+    // Fake test. There should be no task path as this. 
+    // A task always resides in a file.
+    // This is checking implementation: if a task has no project defined with it or in its file, 
+    //      it will find projects in its parent directory.
     test("Root -> Unclassified", () => {
         expect(getProjectNameByProjectPath('/')).toBe('Unclassified')
-    });
+    }); // 
     test("Any file under / -> Unclassified", () => {
         expect(getProjectNameByProjectPath('/file_any')).toBe('Unclassified')
-    });
+    });  // Task (Unclassified). path: `/file_any`
     test("Task def Prj4 under / -> Prj4", () => {
         expect(getProjectNameByProjectPath('/file_any:Prj4')).toBe('Prj4')
-    });
+    }); // Task (Prj4), `/file_any:Prj4`
     test("Any file under Any folder -> Unclassified", () => {
         expect(getProjectNameByProjectPath('/folder_any/file_any')).toBe('Unclassified')
-    });
+    }); 
     test("Folder Prj1 -> Prj1", () => {
         expect(getProjectNameByProjectPath('/folder1/file_any')).toBe('Prj1')
     });
     test("File1 under Prj1 -> Prj1", () => {
         expect(getProjectNameByProjectPath('/folder1/file1')).toBe('Prj1')
-    });
+    }); // File, `tpm_projectroot: Prj1`. `./folder1/file1`
     test("Task in file3 under Prj1 -> Prj1", () => {
         expect(getProjectNameByProjectPath('/folder1/file3')).toBe('Prj1')
-    });
+    }); // Task. (Prj1). `/folder1/file3`
     test("File def Prj2 under Prj1 -> Prj2", () => {
         expect(getProjectNameByProjectPath('/folder1/file2')).toBe('Prj2')
-    });
+    }); // Task. (Prj2)
     test("Task def Prj3 in File 3 -> Prj3", () => {
         expect(getProjectNameByProjectPath('/folder1/file3:Prj3')).toBe('Prj3')
-    });
+    }); // Task, def (Prj3). `/folder1/file3:Prj3`
     test("Any file under Prj3 -> Prj3", () => {
         expect(getProjectNameByProjectPath('/folder1/file4')).toBe('Prj3')
-    })
+    }) // Task (Prj3). `/folder1/file4`
 })
