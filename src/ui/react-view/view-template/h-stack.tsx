924b341c2e636a4b686667614ea51e500fdeed39
oda <odaimoko@qq.com>
2023-11-21 12:22:13 +0800

    add: vstack

--- src/ui/react-view/view-template/h-stack.tsx
+++ src/ui/react-view/view-template/h-stack.tsx
@@ -1,11 +1,14 @@
 import React, {Fragment, ReactNode} from "react";
 
+export interface StyleProps {
+    style?: React.CSSProperties,
+}
+
 // https://react.dev/learn/rendering-lists#keeping-list-items-in-order-with-key
 // JSX elements directly inside a map() call always need keys! 
-interface StackProps {
-    style?: React.CSSProperties,
+interface StackProps extends StyleProps {
     spacing?: number,
-    children: ReactNode[],
+    children?: ReactNode[] | ReactNode
 }
 
 function getSpacingStyle(spacing: number | undefined, isHorizontal = true) {
@@ -20,14 +23,28 @@ function wrapChildrenWithArray(
     return [children];
 }
 
-export function HStack(props: StackProps) {
+function Stack(props: StackProps & { isHorizontal?: boolean }) {
     // style won't override flex and row property of HStack
-    return <div style={Object.assign({}, props.style, {display: "flex", flexDirection: "row"})}>
+    const source2 = {
+        display: "flex",
+        flexDirection: props.isHorizontal ? "row" : "column"
+    };
+    return <div
+        style={Object.assign({}, props.style, source2)}>
         {wrapChildrenWithArray(props.children).map((child: ReactNode, i: number) => {
             return <Fragment key={i}>
-                {i > 0 ? <div style={getSpacingStyle(props.spacing)}/> : null}
+                {i > 0 ? <div style={getSpacingStyle(props.spacing, props.isHorizontal)}/> : null}
                 {child}
             </Fragment>
         })}
     </div>
 }
+
+export function VStack(props: StackProps) {
+    return <Stack {...props} isHorizontal={false}/>
+}
+
+export function HStack(props: StackProps) {
+    return <Stack {...props} isHorizontal={true}/>
+}
+
