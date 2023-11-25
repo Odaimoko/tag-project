576f803f5a03d5a3e3d4d3f4bd191cd002d4e095
oda <934854676@qq.com>
2023-11-25 11:52:10 +0800

    add: deal with checkbox in quartz

--- src/ui/common/tagged-task-view.tsx
+++ src/ui/common/tagged-task-view.tsx
@@ -1,17 +1,34 @@
-import React from "react";
+import React, {useContext} from "react";
 import {HashTagView} from "./hash-tag-view";
+import {MarkdownConvertContext} from "../../utils/markdown-converter";
 
 export const TaggedTaskView = ({content, tags}: {
     content: string,
     tags: string[]
 }) => {
     const checkBoxExampleStyle = {marginTop: 10, marginBottom: 10,}
-
-    return <div style={checkBoxExampleStyle}>
-        <input type={"checkbox"}/>
+    const mcc = useContext(MarkdownConvertContext);
+    const taskContent = <>
         <label>{content} </label>
         {
             tags.map(tag => <HashTagView key={tag} tagWithoutHash={tag}/>)
         }
-    </div>
+    </>;
+
+    return !mcc ?
+        <div style={checkBoxExampleStyle}>
+            <input type={"checkbox"}/>
+            {taskContent}
+        </div> : <ul className="contains-task-list">
+
+            <li className="task-list-item">
+                <input type="checkbox" disabled={true}/>
+                {/* add a space */}
+                <label> </label>
+                {taskContent}
+            </li>
+
+
+        </ul>
+
 }
