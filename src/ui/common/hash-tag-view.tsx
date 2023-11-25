2002332df30ccc931e435c60be440871cd979a24
oda <934854676@qq.com>
2023-11-25 11:15:53 +0800

    fix: wrap `#` with <label> to avoid being recognized as a tag in quartz

--- src/ui/common/hash-tag-view.tsx
+++ src/ui/common/hash-tag-view.tsx
@@ -6,10 +6,13 @@ export const HashTagView = ({tagWithoutHash}: {
 }) => {
     const mcc = useContext(MarkdownConvertContext);
     tagWithoutHash = tagWithoutHash.startsWith("#") ? tagWithoutHash.substring(1) : tagWithoutHash
-    return !mcc ? <>
+    return !mcc ?
+        <>
             <span
                 className="cm-formatting cm-formatting-hashtag cm-hashtag cm-hashtag-begin cm-list-1 cm-meta">#</span>
-        <span
-            className="cm-hashtag cm-hashtag-end cm-list-1 cm-meta ">{tagWithoutHash}</span>
-    </> : <span>    <a className="tag-link internal">#{tagWithoutHash}</a></span>
+            <span
+                className="cm-hashtag cm-hashtag-end cm-list-1 cm-meta ">{tagWithoutHash}</span>
+        </>
+        // To prevent quartz from recognizing this as a tag, we wrap ths hashtag in a label
+        : <span> <a className="tag-link internal"><label>{"#"}</label>{tagWithoutHash}</a></span>
 }
