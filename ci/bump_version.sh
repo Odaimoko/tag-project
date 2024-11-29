80ee68fdc17b57043f3019add6e8fd04438bd5db
Odaimoko <934854676@qq.com>
2024-11-29 23:26:04 +0800

    mod: error check when bumping versions

--- ci/bump_version.sh
+++ ci/bump_version.sh
@@ -8,7 +8,12 @@ if [ ! -f "package.json" ]; then
   echo "Error: should be run from the root of the repo"
   exit 1
 fi
-node ci/version-bump.mjs    
+node ci/version-bump.mjs
+# exit if current-version.txt doesn't exist 
+if [ ! -f "current-version.txt" ]; then
+    echo "Error: current-version.txt not found"
+    exit 1
+fi
 targetVersion=$(<"current-version.txt") # read the current version from the file
 echo "Bumping version to $targetVersion"
 
@@ -17,3 +22,5 @@ git commit -m "Bump version to $targetVersion"
 #git push
 git tag -a "$targetVersion" -m "Bump version tag to $targetVersion"
 #git push --tags
+
+rm current-version.txt
