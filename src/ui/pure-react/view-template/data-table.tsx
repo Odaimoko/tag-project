c06b928a5f79da6cb372a88e1ae4cee4e8f57ccc
Odaimoko <934854676@qq.com>
2025-03-22 01:42:18 +0800

    add: datatable can show only a range of data

--- src/ui/pure-react/view-template/data-table.tsx
+++ src/ui/pure-react/view-template/data-table.tsx
@@ -1,6 +1,19 @@
 import {IRenderable} from "../props-typing/i-renderable";
 import React from "react";
 
+interface DataTableParams {
+    tableTitle: string;
+    headers: IRenderable[];
+    rows: IRenderable[][];
+    onHeaderClicked?: (arg0: number) => void;
+    tableStyle?: React.CSSProperties;
+    thStyle?: React.CSSProperties;
+    thStyleGetter?: (column: number) => React.CSSProperties;
+    cellStyle?: React.CSSProperties;
+    cellStyleGetter?: (column: number, row: number) => React.CSSProperties;
+    rowRange?: [number, number]; // [begin, end], if end is -1, means the end is the last row
+}
+
 /**
  *
  * @param tableTitle
@@ -23,18 +36,11 @@ export const DataTable = ({
                               thStyleGetter,
                               cellStyle,
                               cellStyleGetter,
-                          }: {
-        tableTitle: string,
-        headers: IRenderable[],
-        rows: IRenderable[][],
-        onHeaderClicked?: (arg0: number) => void,
-        tableStyle?: React.CSSProperties,
-        thStyle?: React.CSSProperties,
-        thStyleGetter?: (column: number) => React.CSSProperties,
-        cellStyle?: React.CSSProperties,
-        cellStyleGetter?: (column: number, row: number) => React.CSSProperties
-    }) => {
-
+                              rowRange
+                          }: DataTableParams) => {
+        const start = rowRange?.[0] ?? 0;
+        const end = Math.min(rowRange?.[1] ?? rows.length, rows.length);
+        rows = rows.slice(start, end);
         return (
             <table style={tableStyle} key={tableTitle}>
                 <tbody>
@@ -68,3 +74,10 @@ export const DataTable = ({
             </table>
         );
     }
+
+export const PaginatedDataTable = (props: DataTableParams) => {
+    
+    return (
+        <DataTable {...props}/>
+    )
+}
